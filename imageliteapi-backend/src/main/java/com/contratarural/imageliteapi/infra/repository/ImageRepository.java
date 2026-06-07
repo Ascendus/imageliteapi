8b6eea8fc13b24f0b94ab2bc5b2cd8502394a91b
AscendusEduTec <ascendusthiago@gmail.com>
2026-06-07 12:49:27 -0300

    oi..

--- imageliteapi-backend/src/main/java/com/contratarural/imageliteapi/infra/repository/ImageRepository.java
+++ imageliteapi-backend/src/main/java/com/contratarural/imageliteapi/infra/repository/ImageRepository.java
@@ -1,9 +1,18 @@
 package com.contratarural.imageliteapi.infra.repository;
 
 import com.contratarural.imageliteapi.domain.entity.Image;
+import com.contratarural.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query) {
+        return findAll();
+    }
+
 }
