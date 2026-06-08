2dcc38fdd530a09b8a053c24aa205a8065f7e1b3
AscendusEduTec <ascendusthiago@gmail.com>
2026-06-07 22:36:44 -0300

    ajustar bugs..

--- imageliteapi-backend/src/main/java/com/contratarural/imageliteapi/application/images/ImageController.java
+++ imageliteapi-backend/src/main/java/com/contratarural/imageliteapi/application/images/ImageController.java
@@ -61,7 +61,7 @@ public class ImageController {
             @RequestParam(value = "extension", required = false, defaultValue = "") String extension,
             @RequestParam(value = "query", required = false) String query) {
 
-        var result = imageService.search(ImageExtension.valueOf(extension), query);
+        var result = imageService.search(ImageExtension.ofName(extension), query);
 
         var images = result.stream().map(image -> {
             var url = buildImageURL(image);
@@ -75,7 +75,7 @@ public class ImageController {
     private URI buildImageURL(Image image) {
         String imagePath = "/" + image.getId();
         return ServletUriComponentsBuilder
-                .fromCurrentRequest()
+                .fromCurrentRequestUri()
                 .path(imagePath)
                 .build()
                 .toUri();
