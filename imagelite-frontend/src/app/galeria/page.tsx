a6b8a08b666baf8e6d04cc0b492f28f9e04662fa
AscendusEduTec <ascendusthiago@gmail.com>
2026-06-20 12:11:13 -0300

    consumir api.

--- imagelite-frontend/src/app/galeria/page.tsx
+++ imagelite-frontend/src/app/galeria/page.tsx
@@ -8,19 +8,28 @@ import { useState } from 'react';
 export default function GaleriaPage() {
 
     const useService = useImageService();
-    const [images, setImages] = useState<Image[]> ([])
+    const [images, setImages] = useState<Image[]>([])
 
     async function searchImages() {
         const result = await useService.buscar();
         setImages(result);
-        console.table(result);
+    }
+
+    function renderImageCard(image: Image) {
+        return (
+            <ImageCard nome={image.name} tamanho={image.size} dataUpload={image.uploadDate} src={image.url} />
+        )
+    }
+
+    function renderImageCards() {
+        return images.map(renderImageCard)
     }
 
     return (
         <Template>
             <button className='bg-gray-500' onClick={searchImages}>Clique para carregar imagens</button>
             <section className='grid grid-cols-3 gap-8' >
-                <ImageCard nome='' tamanho='' dataUpload='' src='' />
+                {renderImageCards()}
             </section>
         </Template>
     )
