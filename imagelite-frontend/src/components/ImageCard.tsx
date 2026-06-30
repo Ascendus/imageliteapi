23f80060014b792fd4ec7ec223583bf7e200ebad
AscendusEduTec <ascendusthiago@gmail.com>
2026-06-30 11:56:35 -0300

    converter bits para mb,kb.

--- imagelite-frontend/src/components/ImageCard.tsx
+++ imagelite-frontend/src/components/ImageCard.tsx
@@ -2,15 +2,27 @@
 
 interface ImageCardProps {
     nome?: string;
-    tamanho?: string;
+    tamanho?: number;
     dataUpload?: string;
     src?: string;
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({ nome, tamanho, dataUpload, src }: ImageCardProps) => {
 
-    function download(){
+    function download() {
         window.open(src, '_blank')
+    }
+
+    function formatBytes(bytes: number = 0, decimals = 2) {
+        if (!+bytes) return '0 Bytes'
+
+        const k = 1024
+        const dm = decimals < 0 ? 0 : decimals
+        const sizes = ['Bytes', 'KB', 'MB', 'GB']
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+        return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
     }
 
     return (
@@ -18,7 +30,7 @@ export const ImageCard: React.FC<ImageCardProps> = ({ nome, tamanho, dataUpload,
             <img onClick={download} src={src} className="h-56 w-full object-cover rounded-t-md" alt="" />
             <div className="card-body p-4">
                 <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
-                <p className="text-gray-600">{tamanho}</p>
+                <p className="text-gray-600">{formatBytes(tamanho)}</p>
                 <p className="text-gray-600">{dataUpload}</p>
             </div>
         </div>
